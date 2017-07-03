f4215c02d8ff5efb1d4cf6dd4afc93031a1bcbbb
ceonleo <ceonleo@gmail.com>
2017-07-03 09:29:07 +0530

    Add files via upload

---
+++ Play01.java
@@ -0,0 +1,12 @@
+package player;
+import java.util.Scanner;
+public class Play01 {
+
+	public static void main(String[] args) {
+Scanner sc=new Scanner(System.in);
+String s=sc.next();
+StringBuffer sb=new StringBuffer(s);
+StringBuffer sb1=sb.reverse();
+	System.out.println(sb1);
+	}
+}
